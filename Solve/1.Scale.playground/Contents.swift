ec28f54aa5ee7cf68cefdb749858bb532ce6eec6
jinnify <seungjin429@gmail.com>
2019-09-23 00:51:57 +0900

    solve Blackjack

--- Solve/1.Scale.playground/Contents.swift
+++ Solve/1.Scale.playground/Contents.swift
@@ -1,15 +1,16 @@
 import UIKit
 
-/*
- 음계 문제
- 백준 : 2920문제
- https://www.acmicpc.net/problem/2920
+/*:
+ ### 음계
+ * 백준 : 2920문제
+ * [BAEKJOON](https://www.acmicpc.net/problem/2920)
  */
 
 //"1 2 3 4 5 6 7 8"
 //"8 7 6 5 4 3 2 1"
 //"8 1 7 2 6 3 5 4"
 
+//let list = "8 1 7 2 6 3 5 4".split(separator: " ").map { Int($0)! }
 let list = readLine()!.split(separator: " ").map { Int($0)! }
 
 var ascCase = false
---
+++ Solve/2.Blackjack.playground/Contents.swift
@@ -0,0 +1,39 @@
+import UIKit
+
+/*:
+ ### 블랙잭
+ * 백준 : 2798문제
+ * [BAEKJOON](https://www.acmicpc.net/problem/2798)
+ */
+
+/*
+ 
+ <경우의 수>
+ 
+ n장의 카드에서 3장을 고를경우
+   -> n*(n-1)*(n-2) / 3!
+ 
+ */
+
+//let input = "5 21".split(separator: " ").map { Int($0)! }
+//let data = "5 6 7 8 9".split(separator: " ").map { Int($0)! }
+
+let input = readLine()!.split(separator: " ").map { Int($0)! }
+let data = readLine()!.split(separator: " ").map { Int($0)! }
+
+var maxResult = input.last!
+var result = 0
+
+for i in (0..<data.count) {
+    for j in (i+1..<data.count) {
+        for k in (j+1..<data.count) {
+            let sumValue = data[i] + data[j] + data[k]
+            if sumValue <= maxResult {
+                result = max(result, sumValue)
+            }
+        }
+    }
+}
+
+print(result)
+
