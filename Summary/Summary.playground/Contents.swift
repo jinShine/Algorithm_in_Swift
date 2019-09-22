ac1994b207457fe6c0221267017f335e111371b2
jinnify <seungjin429@gmail.com>
2019-09-23 00:58:39 +0900

    add Tip

--- Summary/Summary.playground/Contents.swift
+++ Summary/Summary.playground/Contents.swift
@@ -1,3 +1,20 @@
 import UIKit
 
-var str = "Hello, playground"
+
+/*:
+ 
+ ## 알고리즘 풀때 키보드 입력방법
+ 
+ 
+ ```swift
+ let value = readLine()
+ ```
+ 
+ * 1 2 3 4 일 경우
+ ```swift
+ let nums = readLine()!.split(separator: " ").map { Int($0)! }
+ 
+ // [1, 2, 3, 4]
+ ```
+ 
+ */
