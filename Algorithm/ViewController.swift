c6862704751051d66ffee1e2f37c3da2cf3c9868
jinnify <seungjin429@gmail.com>
2019-09-22 23:29:12 +0900

    add Scale

---
+++ Algorithm/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Algorithm
+//
+//  Created by seungjin on 2019/09/22.
+//  Copyright © 2019 Jinnify. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Solve/1.Scale.playground/Contents.swift
@@ -0,0 +1,32 @@
+import UIKit
+
+/*
+ 음계 문제
+ 백준 : 2920문제
+ https://www.acmicpc.net/problem/2920
+ */
+
+//"1 2 3 4 5 6 7 8"
+//"8 7 6 5 4 3 2 1"
+//"8 1 7 2 6 3 5 4"
+
+let list = readLine()!.split(separator: " ").map { Int($0)! }
+
+var ascCase = false
+var desCase = false
+
+for i in (1..<list.count) {
+    if list[i - 1] < list[i] {
+        ascCase = true
+    } else if list[i - 1] > list[i] {
+        desCase = true
+    }
+}
+
+if ascCase && desCase {
+    print("mixed")
+} else if ascCase {
+    print("ascending")
+} else {
+    print("descending")
+}
